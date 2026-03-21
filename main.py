1fddbd5b6341be707549282a37acc1d510b8b7ab
EllisSK <ellissinclairkent@gmail.com>
2026-03-21 04:51:10 +0000

    Removed some old test cases

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import numpy as np
 
 from pathlib import Path
 
-from src.SotAMiH.solver.flumes import VariableBedChannel
-from src.SotAMiH.solver.solvers import HLLSolver
-
 from src.SotAMiH.core.simulation import Simulation
 from src.SotAMiH.core.mesh import Mesh1D
 from src.SotAMiH.physics.shallow_water import ShallowWater1D
@@ -14,33 +11,6 @@ from src.SotAMiH.methods.riemann_solvers.hll import HLLSolver
 from src.SotAMiH.core.boundaries import ReflectiveBoundary
 
 def test_case_1():
-    def bed_fn(x, L):
-        return (10) + (40 * x / L) + (10 * np.sin(np.pi * ((4 * x / L) - (1/2))))
-    
-    def initial_depth(x):
-        return 65 -((10) + (40 * x / 14000) + (10 * np.sin(np.pi * ((4 * x / 14000) - (1/2)))))
-    
-    channel = VariableBedChannel(14000, 1, 280, 0, HLLSolver(), bed_fn, initial_depth_func= initial_depth, time_recorders=[1000, 2000, 3000, 4000, 5000], max_time=5000)
-
-    channel.solve_flow(live_view= True)
-
-    channel.write_results(Path("exports/testcase1/results.csv"))
-
-def test_case_2():
-    def bed_fn(x, L):
-        return -0.01*x
-    
-    def initial_depth(x):
-        if x < 10:
-            return 2
-        else:
-            return 10e-6
-    
-    channel = VariableBedChannel(140, 1, 0.5, 0.01, HLLSolver(), bed_function= bed_fn, initial_depth_func=initial_depth)
-
-    channel.solve_flow(live_view= True)
-
-def test_case_3():
     def bed_fn(x,):
         return (10) + (40 * x / 14000) + (10 * np.sin(np.pi * ((4 * x / 14000) - (1/2))))
     
@@ -61,6 +31,13 @@ def test_case_3():
     sim = Simulation(mesh, physics, spatial, temporal, riemann, bcs)
 
     sim.run(5000)
+
+
+def test_case_2():
+    pass
+
+def test_case_3():
+    pass
 
 def test_case_4():
     pass
